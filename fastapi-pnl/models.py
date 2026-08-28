113e8264c830a01e3422f137aa7f4fd5db162b21
Hoang Long <bhlong1911@gmail.com>
2026-08-28 08:11:28 +0700

    Day 99: Add server_default for created_at

--- fastapi-pnl/models.py
+++ fastapi-pnl/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Numeric, DateTime
+from sqlalchemy import Numeric, DateTime, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from decimal import Decimal
 from datetime import datetime
@@ -14,7 +14,7 @@ class Transaction(Base):
     amount: Mapped[Decimal | None] = mapped_column(Numeric())
     price: Mapped[Decimal | None] = mapped_column(Numeric())
     total: Mapped[Decimal | None] = mapped_column(Numeric())
-    created_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+    created_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
         return f"<Transaction(id={self.id}, coin='{self.coin}', action='{self.action}', amount={self.amount}, price={self.price}, total={self.total})>"
